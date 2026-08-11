f090107cbaf58380446755e4dbd68710160468d5
Nks-tech-ctrl <nikhilsingh110104@gmail.com>
2026-08-11 22:14:02 +0530

    fixed hamburger menu open /close

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navlinks = document.querySelector("nav ul");
 hamburger.addEventListener("click",()=>{
     navlinks.classList.toggle("active");
 });
+
+const navitems = document.querySelectorAll("nav ul li a");
+navitems.forEach((item) =>{
+    item.addEventListener("click", () => {
+        navlinks.classList.remove("active");
+    })
+});
