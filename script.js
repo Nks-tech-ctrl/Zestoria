e0efa3e55823bd6cc714275ab1aee5a127fe07e3
Nks-tech-ctrl <nikhilsingh110104@gmail.com>
2026-08-11 22:45:28 +0530

    added javascript form validation

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const hamburger =document.querySelector("#hamburger");
+console.log("working");
+const hamburger = document.querySelector("#hamburger");
 const navlinks = document.querySelector("nav ul");
 
-hamburger.addEventListener("click",()=>{
+hamburger.addEventListener("click", () => {
     navlinks.classList.toggle("active");
 });
 
 const navitems = document.querySelectorAll("nav ul li a");
-navitems.forEach((item) =>{
+navitems.forEach((item) => {
     item.addEventListener("click", () => {
         navlinks.classList.remove("active");
     })
 });
+
+const reservationForm = document.querySelector("#reservation-form");
+console.log(reservationForm);
+reservationForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+
+    const userName = document.querySelector("#name").value.trim();
+    const guests = document.querySelector("#guests").value;
+    const phone = document.querySelector("#phone").value.trim();
+    const date = document.querySelector("#date").value;
+    const time = document.querySelector("#time").value;
+
+    if (!userName || !guests|| !phone || !date || !time) {
+        alert("Please fill details to book table");
+        return;
+    }
+});
+
